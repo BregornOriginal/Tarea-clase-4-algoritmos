dadb2469d17850cb22739ada2eadc533eb406471
BregornOriginal <93630700+BregornOriginal@users.noreply.github.com>
2022-01-06 23:41:24 -0300

    Se agregó ejercicio 19 para completar.

--- tarea-clase-4-algoritmos.js
+++ tarea-clase-4-algoritmos.js
@@ -537,9 +537,12 @@ console.log(imprimirCienNumerosPrimos());
 
 /*
 
-Desafío de programación #19: Crear una función que retorne un array con los primeros números "n-primos" mayores que un número particular definido "alComienzo"
-Ejemplo:
-  Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7,11,13,17,19]
+Desafío de programación #19: Crear una función que retorne un array con los primeros números 
+
+"n-primos" mayores que un número particular definido "alComienzo"
+
+Ejemplo: 
+Quiero los primeros 4 números primos mayores que 5, el resultado debería ser: [7,11,13,17,19]
 
 Desafío de programación #20: Rotar un array hacia la izquierda una posición
 Ejemplo:
